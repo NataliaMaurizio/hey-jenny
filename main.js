42b8058e98221157cb4edd788350b5adf41fc4a3
Maurizio Natalia <maurizio.natalia@techover.it>
2019-04-10 00:14:30 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,82 @@
+$(function() {
+    $("#switch-sm").click(function() {
+
+        if ($('#switch-sm').is(':checked')) {
+            enable();
+        } else {
+            disable();
+        }
+
+    });
+});
+
+chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+    let url = tabs[0].url;
+
+    chrome.cookies.get({url: url, name: 'annyang'}, function (cookie) {
+        if (cookie != null && cookie.value != null && cookie.value == 'true') {
+            $('#switch-sm').attr('checked', 'checked');
+            enable();
+        }
+    });
+
+});
+
+function enable() {
+    // get the url for the current tab
+    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+        let url = tabs[0].url;
+
+        chrome.cookies.set({
+            url: url,
+            name: 'annyang',
+            value: "true"
+        });
+
+    });
+
+    chrome.tabs.getSelected(null, function (tab) {
+        chrome.tabs.executeScript(tab.id, {file: "annyang.js"}, function (response) {
+
+            if (response) {
+                chrome.tabs.executeScript(tab.id, {code: "if (annyang) { annyang.start(); }"}, function (response) {
+                    if (response) {
+
+                        /*tab.navigator.webkitGetUserMedia({ audio: true }, s => {
+                            alert('well done');
+                        }, err => {
+                            alert('ops');
+                        });*/
+
+
+
+                        document.getElementById('info').classList.remove('hidden');
+                        document.getElementById('unlock').classList.remove('hidden');
+
+                    }
+                })
+            }
+
+        });
+    })
+
+}
+
+function disable() {
+
+    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+        let url = tabs[0].url;
+
+        chrome.cookies.remove({
+            url: url,
+            name: 'annyang'
+        });
+
+        document.getElementById('info').classList.add('hidden');
+        document.getElementById('unlock').classList.add('hidden');
+
+    });
+
+
+
+}
